9d832c67cc8dba80ea7efae5e2b95869ae84f8f4
Jarryd Martin <jarryd@codora.co.nz>
2021-06-30 11:25:20 +1200

    added basic drag feature

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { useSpring, animated } from '@react-spring/web'
+import { useDrag } from 'react-use-gesture'
 
 export type itemProps = {
     id: string
@@ -6,10 +8,15 @@ export type itemProps = {
     type: '🍬' | '🍪'
 }
 const Item = (props:itemProps) => {
+    const [{ x, y }, api] = useSpring(() => ({ x: 0, y: 0 }))
+
+    const bind = useDrag(({ down, movement: [mx, my] }) => {
+        api.start({ x: down ? mx : 0, y: down ? my : 0 })
+      })
     return (
-        <div className="Item">
+        <animated.div {...bind()} style={{ x, y }} className="Item">
             {[props.type]} 
-        </div>
+        </animated.div>
     )
 }
 
